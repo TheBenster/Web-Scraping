cb2a92b4f0be0f96350d0ba23540cef933745972
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-04-30 23:21:20 -0400

    add functionality, also kind of broke it. need to look at this tomorrow.

---
+++ website/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dsad'
+    
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+    
+    return app
---
+++ website/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template, request, flash
+import pandas as pd
+from genres import GENRE_FILES
+
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['POST', 'GET'])
+def home():
+    # if request.method == 'POST':
+        
+    #     genre = request.form.get('genre')
+        
+    #     filename = GENRE_FILES.get(genre)
+        
+    #     books = pd.read_csv(filename)
+        
+    #     book = books.sample(n=1).iloc[0]
+        
+    #     return render_template('result.html',  title=book['Title'], author=book['Author'])
+    
+    
+    # return render_template('home.html')
+    return "Hello World"
